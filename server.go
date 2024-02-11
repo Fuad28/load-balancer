c3f0afa8923c01b0b6ec75907ff8394a5ff52f96
Fuad <fuadadio75@gmail.com>
2024-02-12 00:38:35 +0100

    feat: added a custom implementation of NewSingleHostReverseProxy due to an issue with with httputil's

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
 type Server struct {
@@ -16,6 +17,7 @@ type Server struct {
 }
 
 func (s *Server) IsAlive() bool {
+
 	_, err := http.Get(s.healthCheckPath)
 
 	if err != nil {
@@ -48,18 +50,48 @@ func NewDevServer(address string) *Server {
 		healthCheckPath: serverUrl.String(),
 		Address:         serverUrl,
 		ServerMux:       mux,
-		Proxy:           httputil.NewSingleHostReverseProxy(serverUrl),
+		Proxy:           NewSingleHostReverseProxy(serverUrl),
 	}
 }
 
 func NewProdServer(healthCheckPath string, address string) *Server {
-	serverUrl, err := url.Parse("http://" + address)
+	serverUrl, err := url.Parse(address)
 
 	OnErrorPanic(err, "Invalid server address")
 
 	return &Server{
 		healthCheckPath: serverUrl.JoinPath(healthCheckPath).String(),
 		Address:         serverUrl,
-		Proxy:           httputil.NewSingleHostReverseProxy(serverUrl),
+		Proxy:           NewSingleHostReverseProxy(serverUrl),
 	}
+}
+
+// httputil implementation of NewSingleHostReverseProxy doesn't overwrite req.Host
+// If left unmodified, the req.Host defaults to the load balance host.
+func NewSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
