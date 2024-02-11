f023e8ff539afb6adb8a46ebf5d1fd9b624911c5
Fuad <fuadadio75@gmail.com>
2024-02-12 00:34:34 +0100

    refactory: moved lb setup code to a new file

---
+++ setup.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var config LoadBalancerConfig
+
+func setupLoadBalancer() *LoadBalancer {
+
+	_ = config.LoadConfig()
+
+	lb := LoadBalancer{
+		Port:     config.Port,
+		Count:    0,
+		LastPort: config.Port,
+		Config:   config,
+	}
+
+	if config.Env == "dev" {
+		devSetup(&lb)
+	} else if config.Env == "prod" {
+		ProdSetup(&lb)
+	} else {
+		panic("Invalid ENV value")
+	}
+
+	return &lb
+
+}
+
+func devSetup(lb *LoadBalancer) {
+	fmt.Println("DEV SETUP")
+	baseUrl := "127.0.0.1:"
+
+	// create and start demo servers
+	for i := 1; i <= lb.Config.NoOfServers; i++ {
+		nextPort := lb.getNextPort()
+		address := baseUrl + strconv.Itoa(nextPort)
+		server := NewDevServer(address)
+		lb.Servers = append(lb.Servers, server)
+	}
+
+}
+
+func ProdSetup(lb *LoadBalancer) {
+	fmt.Println("PROD SETUP")
+
+	for _, s := range lb.Config.Servers {
+		server := NewProdServer(s.HealthCheckPath, s.Address)
+		lb.Servers = append(lb.Servers, server)
+	}
+}
